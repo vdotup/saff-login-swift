ac8fcc0a009cf9f730b7bf18769050e5b629cd43
vdotup <v.up@live.com>
2022-10-03 17:04:15 +0300

    Initial Commit

---
+++ saff-login-swift/saff_login_swiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  saff_login_swiftApp.swift
+//  saff-login-swift
+//
+//  Created by 7up ‘ on 07/03/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct saff_login_swiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
